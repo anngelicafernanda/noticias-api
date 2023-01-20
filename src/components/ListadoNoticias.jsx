54d54457b68e7b26d00e51e0a219a4e1bb486eb2
anngelicafernanda <devanngelc@gmail.com>
2023-01-20 19:57:25 -0300

    Agregando paginación

--- src/components/ListadoNoticias.jsx
+++ src/components/ListadoNoticias.jsx
@@ -1,5 +1,7 @@
 import Grid  from "@mui/material/Grid";
 import  Typography  from "@mui/material/Grid";
+import  Pagination from "@mui/material/Pagination";
+import  Stack  from "@mui/material/Stack";
 import useNoticias from "../hooks/useNoticias";
 import Noticia from "./Noticia";
 
@@ -28,6 +30,19 @@ const ListadoNoticias = () => {
             />
         ))}
     </Grid>
+    <Stack 
+    sx={{
+        marginY: 5
+    }}
+    spacing={2}
+    direction={"row"}
+    justifyContent="center"
+    alignItems="center"
+    >
+
+    <Pagination count={3} color="primary" />
+
+    </Stack>
     </>
   )
 }
--- src/context/NoticiasProvider.jsx
+++ src/context/NoticiasProvider.jsx
@@ -6,6 +6,9 @@ const NoticiasContext = createContext()
 const NoticiasProvider = ({children}) => {
     const [categoria, setCategoria] = useState("general")
     const [noticias, setNoticias] = useState([])
+    const [pagina, setPagina] =useState(1)
+    const [totalNoticias, setTotalNoticias] =useState(0)
+
     useEffect(() => {
     const consultarAPI = async () => {
         const url = `https://newsapi.org/v2/top-headlines?country=ve&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`
@@ -13,6 +16,7 @@ const NoticiasProvider = ({children}) => {
         const {data} = await axios(url)
 
         setNoticias(data.articles)
+        setTotalNoticias(data.totalResults)
     }
     consultarAPI()
     }, [categoria])
